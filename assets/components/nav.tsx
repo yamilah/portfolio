c46abaecceb11e1533bd738352c419811a2ecdb0
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-09-14 13:51:24 -0700

    Removed extraneous type, added nav styles

--- assets/components/nav.tsx
+++ assets/components/nav.tsx
@@ -4,13 +4,31 @@ import {Link} from "react-router-dom"
 export default class Nav extends React.Component {
   render() {
     return (
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">main</Link>
+      <nav className="nav">
+        <ul className="nav-list">
+          <li className="nav-list-item">
+            <Link
+              className="nav-list-item-link"
+              to="/"
+            >
+              Yamilah Atallah
+            </Link>
           </li>
-          <li>
-            <Link to="/about">about</Link>
+          <li className="nav-list-item nav-list-item--first">
+            <Link
+              className="nav-list-item-link"
+              to="/"
+            >
+              Work
+            </Link>
+          </li>
+          <li className="nav-list-item nav-list-item--last">
+            <Link
+              className="nav-list-item-link"
+              to="/about"
+            >
+              About
+            </Link>
           </li>
         </ul>
       </nav>
