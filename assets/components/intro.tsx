8f4eb45af9b44ffdb818bf508eb77cc92b6cd6c9
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-10-08 18:16:57 -0700

    adsfhadsfhlk

--- assets/components/intro.tsx
+++ assets/components/intro.tsx
@@ -4,36 +4,38 @@ export default class Intro extends React.Component {
   render() {
     return (
       <section className="intro">
-        <h1 className="intro-heading">
-          Yamilah Atallah&nbsp;
-          <span className="intro-heading-emphasis">
-            is a designer and developer in the SF Bay Area.
-          </span>
-        </h1>
-        <ul className="intro-text-list">
-          <li className="intro-text-list-item">
-            it’s pronounced jah • mee • lah btw
-          </li>
-          <li className="intro-text-list-item intro-text-list-item--left">
-            it’s pronounced jah • mee • lah btw
-          </li>
-          <li className="intro-text-list-item intro-text-list-item--left">
-            it’s pronounced jah • mee • lah btw
-          </li>
-          <li className="intro-text-list-item intro-text-list-item--left">
-            it’s pronounced jah • mee • lah btw
-          </li>
-          <li className="intro-text-list-item intro-text-list-item--left">
-            it’s pronounced jah • mee • lah btw
-          </li>
-        </ul>
-        <a href="mailto:info@yamilah.com" className='intro-link--left'>
-          Let’s talk.
-        </a>
+        <div className="intro--inner">
+          <h1 className="intro-heading">
+            Yamilah Atallah&nbsp;
+            <span className="intro-heading-emphasis">
+              is a designer and developer in the SF Bay Area.
+            </span>
+          </h1>
+          <ul className="intro-text-list">
+            <li className="intro-text-list-item">
+              it’s pronounced jah • mee • lah btw
+            </li>
+            <li className="intro-text-list-item intro-text-list-item--left">
+              it’s pronounced jah • mee • lah btw
+            </li>
+            <li className="intro-text-list-item intro-text-list-item--left">
+              it’s pronounced jah • mee • lah btw
+            </li>
+            <li className="intro-text-list-item intro-text-list-item--left">
+              it’s pronounced jah • mee • lah btw
+            </li>
+            <li className="intro-text-list-item intro-text-list-item--left">
+              it’s pronounced jah • mee • lah btw
+            </li>
+          </ul>
+          <a href="mailto:info@yamilah.com" className='intro-link--left'>
+            Let’s talk.
+          </a>
 
-        <p className='intro-link--right'>
-          Work ↓
-        </p>
+          <p className='intro-link--right'>
+            Work ↓
+          </p>
+        </div>
       </section>
     )
   }
