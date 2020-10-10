0883fd2ae657a00331c30bb224d451a13f3b9ef5
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-10-09 22:54:44 -0700

    aaaaaaaaaa

--- assets/components/pages/main.tsx
+++ assets/components/pages/main.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
 import Intro from "../intro"
+import ProjectOne from "../project-one"
 
 export default class Main extends React.Component {
   render() {
     return (
       <div className="main">
         <Intro />
+        <ProjectOne />
       </div>
     )
   }
---
+++ assets/components/project-one.tsx
@@ -0,0 +1,64 @@
+import * as React from "react"
+
+export default class ProjectOne extends React.Component {
+  render() {
+    return (
+      <section className="one">
+        <p className="one-date-1">
+          <span>
+            08
+          </span>
+          <span>
+            09
+          </span>
+          <span>
+            20
+          </span>
+        </p>
+        <p className="one-subtext">
+          <span>
+            Currently:
+          </span>
+          <span>
+            designer,
+          </span>
+          <span>
+            Disney+
+          </span>
+        </p>
+        <p className="one-text-1">
+          Houseparty
+        </p>
+        <p className="one-text-2">
+          2019
+        </p>
+        <p className="one-text-3">
+          social media
+        </p>
+        <p className="one-text-4">
+          & video
+        </p>
+        <p className="one-text-5">
+          chatting web—
+        </p>
+        <p className="one-text-6">
+          app
+        </p>
+        <p className="one-text-7">
+          2015—2020
+        </p>
+        <p className="one-date-2">
+          <span>
+            08
+          </span>
+          <span>
+            09
+          </span>
+          <span>
+            20
+          </span>
+        </p>
+      </section>
+    )
+  }
+}
