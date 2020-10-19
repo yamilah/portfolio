0e9db2fbe1be4b0787f674e8cfb5ab6e02af8c55
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-10-19 15:19:25 -0700

    Added intro markup

--- assets/components/case-one-detail-intro.tsx
+++ assets/components/case-one-detail-intro.tsx
@@ -4,7 +4,36 @@ export default class DetailOneIntro extends React.Component {
   render() {
     return (
       <section className="detail-one-intro">
-        intro
+        <p>
+          2019
+        </p>
+        <p>
+          Houseparty
+        </p>
+        <p>
+          Be
+        </p>
+        <p>
+          You
+        </p>
+        <p>
+          Be
+        </p>
+        <p>
+          There
+        </p>
+        <p>
+          Be
+        </p>
+        <p>
+          Together
+        </p>
+        <p>
+          Where the party at?
+        </p>
+        <p>
+          2019
+        </p>
       </section>
     )
   }
