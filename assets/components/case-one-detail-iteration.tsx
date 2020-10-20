c0dcacc5a7afbf499feeb1ff882194b818af2d2b
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-10-20 16:57:38 -0700

    mv implementation -> iteration

--- assets/components/case-one-detail-iteration.tsx
+++ assets/components/case-one-detail-iteration.tsx
@@ -11,22 +11,22 @@ import vidTwo from "../images/houseparty/houseparty-progress.mp4"
 import vidTwoFallback from "../images/houseparty/houseparty-progress.webm"
 import vidTwoPoster from "../images/houseparty/houseparty-progress-poster.jpg"
 
-export default class DetailOneImplementation extends React.Component {
+export default class DetailOneIteration extends React.Component {
   render() {
     return (
-      <section className="detail-one-implementation">
-        <div className="detail-one-implementation-separator">
-          <p className="detail-one-implementation-separator-number">
+      <section className="detail-one-iteration">
+        <div className="detail-one-iteration-separator">
+          <p className="detail-one-iteration-separator-number">
             02
           </p>
-          <p className="detail-one-implementation-separator-text">
-            Iterations & Implementation
+          <p className="detail-one-iteration-separator-text">
+            Iterations & testing
           </p>
         </div>
-        <div className="detail-one-implementation-prototype">
+        <div className="detail-one-iteration-prototype">
           <video
             autoPlay
-            className="detail-one-implementation-prototype-video dropshadow"
+            className="detail-one-iteration-prototype-video dropshadow"
             loop
             muted
             poster={vidOnePoster}
@@ -40,48 +40,48 @@ export default class DetailOneImplementation extends React.Component {
               type="video/webm"
             />
           </video>
-          <p className="detail-one-implementation-prototype-text">
+          <p className="detail-one-iteration-prototype-text">
             From this work over that week, I was able to lead the conversation to the ultimate goal of the week: a prototype to test against users, an excerpt from which you’re seeing above. Here were some key findings from this testing:
           </p>
         </div>
-        <div className="detail-one-implementation-figures">
-          <figure className="detail-one-implementation-figure-1">
-            <p className="detail-one-implementation-figure-number">
+        <div className="detail-one-iteration-figures">
+          <figure className="detail-one-iteration-figure-1">
+            <p className="detail-one-iteration-figure-number">
               3/5
             </p>
-            <p className="detail-one-implementation-figure-text">
+            <p className="detail-one-iteration-figure-text">
               participants could not figure out where notifications were
             </p>
           </figure>
-          <figure className="detail-one-implementation-figure-2">
-            <p className="detail-one-implementation-figure-number">
+          <figure className="detail-one-iteration-figure-2">
+            <p className="detail-one-iteration-figure-number">
               5/5
             </p>
-            <p className="detail-one-implementation-figure-text">
+            <p className="detail-one-iteration-figure-text">
               participants could figure out how to start a chat
             </p>
           </figure>
-          <figure className="detail-one-implementation-figure-3">
-            <p className="detail-one-implementation-figure-number">
+          <figure className="detail-one-iteration-figure-3">
+            <p className="detail-one-iteration-figure-number">
               4/5
             </p>
-            <p className="detail-one-implementation-figure-text">
+            <p className="detail-one-iteration-figure-text">
               participants could figure out how to add a friend
             </p>
           </figure>
-          <figure className="detail-one-implementation-figure-4">
-            <p className="detail-one-implementation-figure-number">
+          <figure className="detail-one-iteration-figure-4">
+            <p className="detail-one-iteration-figure-number">
               5/5
             </p>
-            <p className="detail-one-implementation-figure-text">
+            <p className="detail-one-iteration-figure-text">
               participants understood the wave feature
             </p>
           </figure>
         </div>
-        <div className="detail-one-implementation-progress">
+        <div className="detail-one-iteration-progress">
           <video
             autoPlay
-            className="detail-one-implementation-progress-video dropshadow"
+            className="detail-one-iteration-progress-video dropshadow"
             loop
             muted
             poster={vidTwoPoster}
@@ -95,40 +95,40 @@ export default class DetailOneImplementation extends React.Component {
               type="video/webm"
             />
           </video>
-          <p className="detail-one-implementation-progress-text-1">
+          <p className="detail-one-iteration-progress-text-1">
             With these results in mind, we felt good about the scope we selected for our MVP and got started. Being the sole designer, I focused on bringing the agreed upon feature set from sketches, to lofi prototypes, testing those with users, and bringing them to hifi—which is what you’re seeing here.
           </p>
-          <p className="detail-one-implementation-progress-text-2">
+          <p className="detail-one-iteration-progress-text-2">
             While doing this, I was also implementing designs and components on the front-end in storybook, and then having devs pick up those storybook components up, hooking those up to data from Houseparty’s API. I certainly had my hands full! We all did.
           </p>
         </div>
-        <div className="detail-one-implementation-ideas">
+        <div className="detail-one-iteration-ideas">
           <img
             alt="A product concept drawing"
-            className="detail-one-implementation-idea-1 dropshadow"
+            className="detail-one-iteration-idea-1 dropshadow"
             src={imgFive}
           />
           <img
             alt="A product concept drawing"
-            className="detail-one-implementation-idea-2 dropshadow"
+            className="detail-one-iteration-idea-2 dropshadow"
             src={imgFour}
           />
           <img
             alt="A product concept drawing"
-            className="detail-one-implementation-idea-3 dropshadow"
+            className="detail-one-iteration-idea-3 dropshadow"
             src={imgThree}
           />
           <img
             alt="A product concept drawing"
-            className="detail-one-implementation-idea-4 dropshadow"
+            className="detail-one-iteration-idea-4 dropshadow"
             src={imgTwo}
           />
           <img
             alt="A product concept drawing"
-            className="detail-one-implementation-idea-5 dropshadow"
+            className="detail-one-iteration-idea-5 dropshadow"
             src={imgOne}
           />
-          <p className="detail-one-implementation-idea-text">
+          <p className="detail-one-iteration-idea-text">
             As it happens, what you see on production, <a href="https://app.houseparty.com">app.houseparty.com</a>, is really just a small slice of the many iterations of designs I put together and tried out. I felt like we really did our due diligence when working on where to put all the controls and interactions. What you’re seeing here are some examples that we looked into.
           </p>
         </div>
--- assets/components/pages/case-detail-one.tsx
+++ assets/components/pages/case-detail-one.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import DetailOneDescription from "../case-one-detail-description"
-import DetailOneImplementation from "../case-one-detail-implementation"
 import DetailOneIntro from "../case-one-detail-intro"
+import DetailOneIteration from "../case-one-detail-iteration"
 import DetailOnePlanning from "../case-one-detail-planning"
 
 export default class CaseDetailOne extends React.Component {
@@ -11,7 +11,7 @@ export default class CaseDetailOne extends React.Component {
         <DetailOneIntro />
         <DetailOneDescription />
         <DetailOnePlanning />
-        <DetailOneImplementation />
+        <DetailOneIteration />
       </section>
     )
   }
