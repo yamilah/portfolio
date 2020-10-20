66d8a4ce180fcbf227feb33755506a213550cf8a
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-10-20 10:56:58 -0700

    responsive planning

--- assets/components/case-one-detail-planning.tsx
+++ assets/components/case-one-detail-planning.tsx
@@ -38,16 +38,22 @@ export default class DetailOnePlanning extends React.Component {
             </p>
           </figure>
         </div>
-        <div className="detail-one-planning-text-wrapper">
-          <p className="detail-one-planning-text-1">
-            From this, we learned that there could be an opportunity for a web experience for Houseparty.
-          </p>
-          <p className="detail-one-planning-text-2">
-            I began at the problem, but I want to zoom out and show the solution I designed to give you a better idea of what we were working towards.
-          </p>
-          <p className="detail-one-planning-text-3">
-            So here’s how you use Houseparty: you sign up, log in, add a friend, jump in their “room,” and have a chat. Seem simple?
-          </p>
+        <div className="detail-one-planning-solution">
+          <div className="detail-one-planning-solution-text-wrapper">
+            <p className="detail-one-planning-solution-text-1">
+              From this, we learned that there could be an opportunity for a web experience for Houseparty.
+            </p>
+            <p className="detail-one-planning-solution-text-2">
+              I began at the problem, but I want to zoom out and show the solution I designed to give you a better idea of what we were working towards.
+            </p>
+            <p className="detail-one-planning-solution-text-3">
+              So here’s how you use Houseparty: you sign up, log in, add a friend, jump in their “room,” and have a chat. Seem simple?
+            </p>
+          </div>
+          <img
+            className="detail-one-planning-solution-img"
+            src="http://placekitten.com/2000/1000"
+          />
         </div>
       </section>
     )
