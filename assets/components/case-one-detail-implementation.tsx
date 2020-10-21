43ada9a145191e39e9350b25fbe0b6fd2993bcaa
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-10-20 22:10:49 -0700

    Added focus

--- assets/components/case-one-detail-implementation.tsx
+++ assets/components/case-one-detail-implementation.tsx
@@ -1,4 +1,7 @@
 import * as React from "react"
+import imgOne from "../images/houseparty/houseparty-device-1.jpg"
+import imgTwo from "../images/houseparty/houseparty-device-2.jpg"
+import imgThree from "../images/houseparty/houseparty-focus.jpg"
 import vidOne from "../images/houseparty/houseparty-performance-1.mp4"
 import vidOnePoster from "../images/houseparty/houseparty-performance-poster-1.jpg"
 import vidTwo from "../images/houseparty/houseparty-performance-2.mp4"
@@ -48,6 +51,37 @@ export default class DetailOneImplementation extends React.Component {
           </p>
           <p className="detail-one-implementation-prototype-text-2">
             What you’re seeing here are some actual prototypes I made to explore these pain points around timing, ordering, responsiveness—and all of this at different screen sizes and orientations.
+          </p>
+        </div>
+        <div className="detail-one-implementation-devices">
+          <img
+            alt="A small Chromebook"
+            className="detail-one-implementation-device-1 dropshadow"
+            src={imgOne}
+          />
+          <img
+            alt="A luxury Chromebook"
+            className="detail-one-implementation-device-2 dropshadow"
+            src={imgTwo}
+          />
+          <p className="detail-one-implementation-device-text">
+            This is because Chromebooks, our target OS and devices, have dramatic differences in their performance levels and screen sizes, and gyroscope support (landscape and portrait). Getting this feedback from our users about the chat latency and responsiveness, I took on the design and technical challenge of figuring out this very complex part of the product. And here’s how I did that:
+          </p>
+        </div>
+        <div className="detail-one-implementation-focus">
+          <img
+            alt="An error page showing that this app can only be used on Chrome"
+            className="detail-one-implementation-focus-img dropshadow"
+            src={imgThree}
+          />
+          <p className="detail-one-implementation-focus-text-1">
+            From the beginning, we agreed we wouldn’t support browsers like Internet Explorer, some messaging around this you’re seeing here. But the upshot to this choice is focus:
+          </p>
+          <p className="detail-one-implementation-focus-text-2">
+            I could leverage cutting edge browser standards, such as CSS Grid, which wasn’t available on Internet Explorer.
+          </p>
+          <p className="detail-one-implementation-focus-text-3">
+            This was much to the CTO’s chagrin, who felt very strongly that JavaScript should do the heavy lifting.
           </p>
         </div>
       </section>
