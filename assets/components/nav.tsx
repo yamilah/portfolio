3be3e68bea53c95c5e246f433c13d9a300244067
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-10-22 12:27:32 -0700

    refactoring nav

--- assets/components/nav.tsx
+++ assets/components/nav.tsx
@@ -4,40 +4,45 @@ import {Link} from "react-router-dom"
 export default class Nav extends React.Component {
   render() {
     return (
-      <nav className="nav">
-        <ul className="nav-list">
-          <li className="nav-list-item">
-            <Link
-              className="nav-link nav-link--first"
-              to="/"
-            >
-              Yamilah Atallah
-              <span className="nav-link-subtext">
-                jah • mee • lah
-              </span>
-            </Link>
-          </li>
-          <li className="nav-list-item nav-list-link">
-            <a
-              className="nav-link"
-              href="https://bit.ly/atallah-cv"
-            >
-              CV↗
-            </a>
-          </li>
-          <li className="nav-list-item nav-list-link">
-            <Link
-              className="nav-link"
-              to="/about"
-            >
-              About
-            </Link>
-          </li>
-          <li className="nav-list-item nav-list-menu">
-            menu
-          </li>
-        </ul>
-      </nav>
+      <div className="nav-wrapper">
+        <nav className="nav">
+          <Link
+            className="nav-link nav-link--home"
+            to="/"
+          >
+            Yamilah Atallah
+          </Link>
+          <ul className="nav-list">
+            <li className="nav-list-item">
+              <a
+                className="nav-link"
+                href="https://bit.ly/atallah-cv"
+              >
+                CV↗
+              </a>
+            </li>
+            <li className="nav-list-item">
+              <Link
+                className="nav-link"
+                to="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-list-item nav-list-menu">
+              menu
+            </li>
+          </ul>
+        </nav>
+        <div
+          className="nav-marquee"
+          role="banner"
+        >
+          <p className="nav-marquee-text">
+            It’s jah • mee • lah btw
+          </p>
+        </div>
+      </div>
     )
   }
 }
