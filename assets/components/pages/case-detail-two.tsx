ba61e684b82d02ec8db233216fee0d516afc299b
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-12-02 11:31:36 -0800

    Dropped some content in detail two.

--- assets/components/pages/case-detail-two.tsx
+++ assets/components/pages/case-detail-two.tsx
@@ -1,12 +1,23 @@
 import * as React from "react"
+import DetailOneDescription from "../case-one-detail-description"
+import DetailOneFinale from "../case-one-detail-one-finale"
+import DetailOneImplementation from "../case-one-detail-implementation"
+import DetailOneIntro from "../case-one-detail-intro"
+import DetailOneIteration from "../case-one-detail-iteration"
+import DetailOnePlanning from "../case-one-detail-planning"
+import Footer from "../footer"
 
 export default class CaseDetailTwo extends React.Component {
   render() {
     return (
-      <section className="case-detail-two">
-        <p className="case-detail-two-text">
-          Coming soon!
-        </p>
+      <section className="case-detail-one">
+        <DetailOneIntro />
+        <DetailOneDescription />
+        <DetailOnePlanning />
+        <DetailOneIteration />
+        <DetailOneImplementation />
+        <DetailOneFinale />
+        <Footer />
       </section>
     )
   }
