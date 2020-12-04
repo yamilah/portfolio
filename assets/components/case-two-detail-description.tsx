cda82db7c894b65c73cdcb0fdcbfdf4e9dfb1517
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-12-04 12:27:43 -0800

    Added new description

--- assets/components/case-two-detail-description.tsx
+++ assets/components/case-two-detail-description.tsx
@@ -10,7 +10,7 @@ export default class DetailTwoDescription extends React.Component {
               Opportunity
             </h1>
             <p className="detail-two-highlight-text">
-              Instantaneous video chatting of up to 8 people on the web
+              Prediction-based companion app to sports games
             </p>
           </div>
           <div className="detail-two-highlight">
@@ -18,13 +18,13 @@ export default class DetailTwoDescription extends React.Component {
               Challenges
             </h1>
             <p className="detail-two-highlight-text">
-              Short timeline
+              Big pivots
             </p>
             <p className="detail-two-highlight-text">
-              Video chat tech
+              React Native
             </p>
             <p className="detail-two-highlight-text">
-              New product
+              iOS & Android
             </p>
           </div>
           <div className="detail-two-highlight">
@@ -32,13 +32,16 @@ export default class DetailTwoDescription extends React.Component {
               Team
             </h1>
             <p className="detail-two-highlight-text">
-              Me (sole designer & front-end dev)
+              Me (designer & front-end dev)
+            </p>
+            <p className="detail-two-highlight-text">
+              1 other designer & developer
             </p>
             <p className="detail-two-highlight-text">
               2 developers
             </p>
             <p className="detail-two-highlight-text">
-              CTO & PM
+              2 PMs
             </p>
           </div>
           <div className="detail-two-highlight">
@@ -46,19 +49,19 @@ export default class DetailTwoDescription extends React.Component {
               Duration
             </h1>
             <p className="detail-two-highlight-text">
-              3 months
+              5 months
             </p>
           </div>
         </div>
         <div className="detail-two-text-wrapper">
           <p className="detail-two-text">
-            Houseparty is a face-to-face social network focuses on instant video chats of up to 8 people. I really loved working on this one—it was a short timeline, but I had a ton of fun working with the problems synchronous communication tends to require. Check it out at <a href="https://app.houseparty.com">app.houseparty.com</a>!
+            HOFR, or Hall of Famer, is a sports prediction mobile app, and you use it while watching a game. There were so many unexpected twists and turns on this project! I was also using React Native professionally for the first time.
           </p>
           <p className="detail-two-text">
-            I was the only designer and front-end developer on the team, but there were two other devs, a CTO, and the lead PM at the company. We had a little over three months.
+            I was one of 2 designers and front-end developers on the team, and it was about 5 months long.
           </p>
           <p className="detail-two-text">
-            To start things off, I’d like to elaborate more on the problem and why we wanted to make this thing. The team at Houseparty said they really wanted to work with us on designing and building out a web experience. Working with them, we were able to tap into some research to see about why. Here were some key excerpts:
+            To start things off, I'd like to provide a little context into how the product works. You can check it out on the <a href="https://apps.apple.com/us/app/hofr/id1400605641">App Store</a> or the <a href="https://play.google.com/store/apps/details?id=com.hofr">Play Store</a>—or just read on!
           </p>
         </div>
       </section>
