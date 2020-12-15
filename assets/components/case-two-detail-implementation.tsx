a369b9f50311b591eef48a0f23d07263db950bd8
Yamilah Atallah <yamilah.atallah@gmail.com>
2020-12-15 06:43:13 -0800

    added implementation content, updated typescript

--- assets/components/case-two-detail-implementation.tsx
+++ assets/components/case-two-detail-implementation.tsx
@@ -1,14 +1,4 @@
 import * as React from "react"
-import imgFour from "../images/houseparty/houseparty-snippet.jpg"
-import imgOne from "../images/houseparty/houseparty-device-1.jpg"
-import imgThree from "../images/houseparty/houseparty-focus.jpg"
-import imgTwo from "../images/houseparty/houseparty-device-2.jpg"
-import vidOne from "../images/houseparty/houseparty-performance-1.mp4"
-import vidOneFallback from "../images/houseparty/houseparty-performance-1.webm"
-import vidOnePoster from "../images/houseparty/houseparty-performance-poster-1.jpg"
-import vidTwo from "../images/houseparty/houseparty-performance-2.mp4"
-import vidTwoFallback from "../images/houseparty/houseparty-performance-2.webm"
-import vidTwoPoster from "../images/houseparty/houseparty-performance-poster-2.jpg"
 
 export default class DetailTwoImplementation extends React.Component {
   render() {
@@ -22,94 +12,44 @@ export default class DetailTwoImplementation extends React.Component {
             A second version
           </p>
         </div>
-        <div className="detail-two-implementation-prototype">
-          <video
-            autoPlay
-            className="detail-two-implementation-prototype-video-1 dropshadow"
-            loop
-            muted
-            poster={vidOnePoster}
-          >
-            <source
-              src={vidOne}
-              type="video/mp4"
-            />
-            <source
-              src={vidOneFallback}
-              type="video/webm"
-            />
-            Sorry, but your browser doesn't support HTML5 videos. Try on another!
-          </video>
-          <video
-            autoPlay
-            className="detail-two-implementation-prototype-video-2 dropshadow"
-            loop
-            muted
-            poster={vidTwoPoster}
-          >
-            <source
-              src={vidTwo}
-              type="video/mp4"
-            />
-            <source
-              src={vidTwoFallback}
-              type="video/webm"
-            />
-            Sorry, but your browser doesn't support HTML5 videos. Try on another!
-          </video>
-          <p className="detail-two-implementation-prototype-text-1">
-            We were all moving really quickly, so we didn't expect what we learned next: users struggled a ton with prototype issues that came with all the video reordering—this was because we were using JavaScript heavily to handle all that.
-          </p>
-          <p className="detail-two-implementation-prototype-text-2">
-            What you’re seeing here are some actual prototypes I made to explore these pain points around timing, ordering, responsiveness—and all of this at different screen sizes and orientations.
-          </p>
+        <div className="detail-two-implementation-solution">
+          <div className="detail-two-implementation-solution-text-wrapper">
+            <p className="detail-two-implementation-solution-text-1">
+              So, we agreed on a V2; we also needed to agree on what the priorities would be for that V2 work.
+            </p>
+            <p className="detail-two-implementation-solution-text-2">
+              For me, what would mostly decide that would be what our users were talking about the most. So, to get some insight into that, I did some data analysis on the reviews in the App Store and the Play Store.
+            </p>
+            <p className="detail-two-implementation-solution-text-3">
+              Here were some key takeaways:
+            </p>
+          </div>
         </div>
-        <div className="detail-two-implementation-devices">
-          <img
-            alt="A small Chromebook"
-            className="detail-two-implementation-device-1 dropshadow"
-            src={imgOne}
-          />
-          <img
-            alt="A luxury Chromebook"
-            className="detail-two-implementation-device-2 dropshadow"
-            src={imgTwo}
-          />
-          <p className="detail-two-implementation-device-text">
-            It was important to considre this as, Chromebooks,—our target OS and device type—have dramatic differences in performance levels and screen sizes, gyroscope support (landscape and portrait), etc. Getting this feedback from our users about the chat latency and responsiveness, I took on the design and technical challenge of figuring out this very complex part of the product. And here’s how I did that:
-          </p>
-        </div>
-        <div className="detail-two-implementation-focus">
-          <img
-            alt="An error page showing that this app can only be used on Chrome"
-            className="detail-two-implementation-focus-img dropshadow"
-            src={imgThree}
-          />
-          <p className="detail-two-implementation-focus-text-1">
-            From the beginning, we agreed we wouldn’t support browsers like Internet Explorer, some messaging around this you’re seeing here. But the upshot to this choice is focus:
-          </p>
-          <p className="detail-two-implementation-focus-text-2">
-            I could leverage cutting edge browser standards, such as CSS Grid, which wasn’t available on Internet Explorer.
-          </p>
-          <p className="detail-two-implementation-focus-text-3">
-            This was much to the CTO’s chagrin, who felt very strongly that JavaScript should do the heavy lifting.
-          </p>
-        </div>
-        <div className="detail-two-implementation-snippet">
-          <img
-            alt="A verbose code snippet"
-            className="detail-two-implementation-snippet-img dropshadow"
-            src={imgFour}
-          />
-          <p className="detail-two-implementation-snippet-text-1">
-            But I have a different philosophy: I see styling as design and, when design doesn’t live in Figma, it should live in its web analog—CSS.
-          </p>
-          <p className="detail-two-implementation-snippet-text-2">
-            Risky choice but, luckily, one that panned out; it enabled an experience that was responsive, performant, way more maintainable, contrasted to the JavaScript route.
-          </p>
-          <p className="detail-two-implementation-snippet-text-3">
-            These buttery-smooth transitions added polish but, more importantly, they closed that feedback loop users were struggling with, making it way easier to see everyone while chatting—we received feedback that immersion was way improved.
-          </p>
+        <div className="detail-two-implementation-figures">
+          <figure className="detail-two-implementation-figure-1">
+            <p className="detail-two-implementation-figure-number">
+              35%
+            </p>
+            <p className="detail-two-implementation-figure-text">
+              of feedback mentioned confusing home states
+            </p>
+          </figure>
+          <figure className="detail-two-implementation-figure-2">
+            <p className="detail-two-implementation-figure-number">
+              42%
+            </p>
+            <p className="detail-two-implementation-figure-text">
+              of feedback mentioned a desire for notifications
+            </p>
+          </figure>
+          <figure className="detail-two-implementation-figure-3">
+            <p className="detail-two-implementation-figure-number">
+              9%
+            </p>
+            <p className="detail-two-implementation-figure-text">
+              of feedback mentioned not knowing when the next games might be
+            </p>
+          </figure>
         </div>
       </section>
     )
