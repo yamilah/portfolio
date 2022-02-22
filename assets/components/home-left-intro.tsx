b41f5757a57819b6624f03b6a3b0ecac3652d54f
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-02-22 11:20:34 -0800

    getting ready to add scribble hah

--- assets/components/home-left-intro.tsx
+++ assets/components/home-left-intro.tsx
@@ -4,26 +4,28 @@ export default class HomeLeftIntro extends React.Component {
   render() {
     return (
       <div className="home-left-intro">
-        <h1 className="home-left-intro-first">
-          Yamilah
-        </h1>
-        <h2 className="home-left-intro-last">
-          Atallah
-        </h2>
-        <p className="home-left-intro-phonetic">
-          <span className="home-left-intro-phonetic-1">
-            jah
-          </span>
-          <span className="home-left-intro-phonetic-2">
-            mee
-          </span>
-          <span className="home-left-intro-phonetic-3">
-            lah
-          </span>
-        </p>
-        <p className="home-left-intro-text">
-          Hi! I’m a designer and front-end developer based in the Bay Area. Currently, I’m working at a certain social media company.
-        </p>
+        <div className="home-left-intro-content">
+          <h1 className="home-left-intro-first">
+            Yamilah
+          </h1>
+          <h2 className="home-left-intro-last">
+            Atallah
+          </h2>
+          <p className="home-left-intro-phonetic">
+            <span className="home-left-intro-phonetic-1">
+              jah
+            </span>
+            <span className="home-left-intro-phonetic-2">
+              mee
+            </span>
+            <span className="home-left-intro-phonetic-3">
+              lah
+            </span>
+          </p>
+          <p className="home-left-intro-text">
+            Hi! I’m a designer and front-end developer based in the Bay Area. Currently, I’m working at a certain social media company.
+          </p>
+        </div>
       </div>
     )
   }
