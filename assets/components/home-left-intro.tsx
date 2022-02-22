8c1eb73fac05bdb00099448fa46ac189fa32e174
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-02-21 17:02:14 -0800

    added broad strokes of the first panel

--- assets/components/home-left-intro.tsx
+++ assets/components/home-left-intro.tsx
@@ -10,6 +10,20 @@ export default class HomeLeftIntro extends React.Component {
         <h2 className="home-left-intro-last">
           Atallah
         </h2>
+        <p className="home-left-intro-phonetic">
+          <span className="home-left-intro-phonetic-1">
+            jah
+          </span>
+          <span className="home-left-intro-phonetic-2">
+            mee
+          </span>
+          <span className="home-left-intro-phonetic-3">
+            lah
+          </span>
+        </p>
+        <p className="home-left-intro-text">
+          Hi! I’m a designer and front-end developer based in the Bay Area. Currently, I’m working at a certain social media company.
+        </p>
       </div>
     )
   }
