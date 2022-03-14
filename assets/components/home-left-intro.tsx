a94c531f8068dfd0faf86b9c5b3c4b215c5659d2
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-03-14 14:30:16 -0700

    added a line lol

--- assets/components/home-left-intro.tsx
+++ assets/components/home-left-intro.tsx
@@ -1,9 +1,15 @@
 import * as React from "react"
+import imgOne from "../images/home-left-intro-drawing.svg"
 
 export default class HomeLeftIntro extends React.Component {
   render() {
     return (
       <div className="home-left-intro">
+        <img
+          alt="A decorative line"
+          className="home-left-intro-line"
+          src={imgOne}
+        />
         <div className="home-left-intro-content">
           <h1 className="home-left-intro-first">
             Yamilah
