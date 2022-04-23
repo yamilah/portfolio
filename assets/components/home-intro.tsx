55ebd2ad5a01302825bb730f4ac9c88b5526ee94
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-04-23 12:35:40 -0700

    okay, finally got it going again

--- assets/components/home-intro.tsx
+++ assets/components/home-intro.tsx
@@ -3,29 +3,27 @@ import * as React from "react"
 export default class HomeLIntro extends React.Component {
   render() {
     return (
-      <div className="home-left-intro">
-        <div className="home-left-intro-content">
-          <h1 className="home-left-intro-first">
-            Yamilah
-          </h1>
-          <h2 className="home-left-intro-last">
-            Atallah
-          </h2>
-          <p className="home-left-intro-phonetic">
-            <span className="home-left-intro-phonetic-1">
-              jah
-            </span>
-            <span className="home-left-intro-phonetic-2">
-              mee
-            </span>
-            <span className="home-left-intro-phonetic-3">
-              lah
-            </span>
-          </p>
-          <p className="home-left-intro-text">
-            Hi! I’m a designer and front-end developer based in the Bay Area. Currently, I’m working at a certain social media company.
-          </p>
-        </div>
+      <div className="home-intro">
+        <h1 className="home-intro-first">
+          Yamilah
+        </h1>
+        <h2 className="home-intro-last">
+          Atallah
+        </h2>
+        <p className="home-intro-phonetic">
+          <span className="home-intro-phonetic-1">
+            jah
+          </span>
+          <span className="home-intro-phonetic-2">
+            mee
+          </span>
+          <span className="home-intro-phonetic-3">
+            lah
+          </span>
+        </p>
+        <p className="home-intro-text">
+          Hi! I’m a designer and front-end developer based in the Bay Area. Currently, I’m working at a certain social media company.
+        </p>
       </div>
     )
   }
