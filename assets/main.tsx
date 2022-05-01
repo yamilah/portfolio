e0aad18fce4b23a8c0e83b93378b816687e2297a
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-04-30 18:40:21 -0700

    switched to createroot

--- assets/main.tsx
+++ assets/main.tsx
@@ -1,20 +1,22 @@
 import * as React from "react"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import * as ReactDOM from "react-dom"
+import { createRoot } from 'react-dom/client'
 import Home from "./js/pages/home"
 import CaseOne from "./js/pages/case-one"
 
 class Root extends React.Component {
   render() {
-    return <Router>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/houseparty" element={<CaseOne />}></Route>
-      </Routes>
-    </Router>
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/houseparty" element={<CaseOne />}></Route>
+        </Routes>
+      </Router>
+    )
   }
 }
 
 window.addEventListener("DOMContentLoaded", event => {
-  ReactDOM.render(<Root />, document.getElementById("root"))
+  createRoot(document.getElementById("root")).render(<Root />)
 })
