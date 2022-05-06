5ca4e7ae6319a0bd4244cfc889a84864b9aeee96
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-05-06 06:46:47 -0700

    Added houseparty case study details

--- assets/js/pages/case-one.tsx
+++ assets/js/pages/case-one.tsx
@@ -1,7 +1,10 @@
 import * as React from "react"
 import CaseOneDescription from "../components/case-one-description"
 import CaseOneFinale from "../components/case-one-finale"
+import CaseOneImplementation from "../components/case-one-implementation"
 import CaseOneIntro from "../components/case-one-intro"
+import CaseOneIteration from "../components/case-one-iteration"
+import CaseOnePlanning from "../components/case-one-planning"
 
 export default class CaseOne extends React.Component {
   render() {
@@ -9,6 +12,9 @@ export default class CaseOne extends React.Component {
       <div>
         <CaseOneIntro></CaseOneIntro>
         <CaseOneDescription></CaseOneDescription>
+        <CaseOnePlanning></CaseOnePlanning>
+        <CaseOneIteration></CaseOneIteration>
+        <CaseOneImplementation></CaseOneImplementation>
         <CaseOneFinale></CaseOneFinale>
       </div>
 
