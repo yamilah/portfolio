bb22f96499d032d75a1874d977d9adaeeae2b89b
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-06-23 08:49:32 -0700

    completely restarting on the styling

--- assets/js/components/home-case-one.tsx
+++ assets/js/components/home-case-one.tsx
@@ -1,65 +1,21 @@
 import * as React from "react"
 import {Link} from "react-router-dom"
-import imgOne from "../../images/home-case-one-line-1.svg"
-import imgThree from "../../images/home-case-one-line-3.svg"
-import imgFour from "../../images/home-case-one-line-4.svg"
 
 export default class HomeCaseOne extends React.Component {
   render() {
     return (
       <section className="home-case-one">
-        <img
-          alt="A decorative line"
-          className="home-case-one-line-1"
-          src={imgOne}
-        />
-        <img
-          alt="A decorative line"
-          className="home-case-one-line-2"
-          src={imgFour}
-        />
         <h1 className="home-case-one-heading-1">
-          hous
-          <span className="home-case-one-heading-1-detail">
-            e
-          </span>
+          house
         </h1>
         <h1 className="home-case-one-heading-2">
-          part
-          <span className="home-case-one-heading-2-detail">
-            y
-          </span>
+          party
         </h1>
-        <div className="home-case-one-text-wrapper">
-          <p className="home-case-one-text-paragraph">
-            <span className="home-case-one-text-1">
-              social
-            </span>
-            <span className="home-case-one-text-2">
-              media
-            </span>
-            <span className="home-case-one-text-3">
-              &
-            </span>
-          </p>
-          <p className="home-case-one-text-paragraph">
-            <span className="home-case-one-text-4">
-              video
-            </span>
-            <span className="home-case-one-text-5">
-              chatting
-            </span>
-          </p>
-        </div>
         <Link
           className="home-case-one-link"
           to="/houseparty"
         >
-          <img
-            alt="A decorative image link that reads: Check it out"
-            className="home-case-one-line-3"
-            src={imgThree}
-          />
+          View this study
         </Link>
       </section>
     )
--- assets/js/components/home-case-two.tsx
+++ assets/js/components/home-case-two.tsx
@@ -1,57 +1,16 @@
 import * as React from "react"
 import {Link} from "react-router-dom"
-import imgOne from "../../images/home-case-two-line-1.svg"
-import imgTwo from "../../images/home-case-one-line-3.svg"
 
 export default class HomeCaseTwo extends React.Component {
   render() {
     return (
       <section className="home-case-two">
-        <img
-          alt="A decorative line"
-          className="home-case-two-line-1"
-          src={imgOne}
-        />
-        <div className="home-case-two-headings">
-          <h1 className="home-case-two-heading-1">
-            <span className="home-case-two-heading-span-1">
-              hall
-            </span>
-            <span className="home-case-two-heading-span-2">
-              of
-            </span>
-          </h1>
-          <h1 className="home-case-two-heading-2">
-            famer
-          </h1>
-        </div>
-        <div className="home-case-two-texts">
-          <p className="home-case-two-text-1">
-            <span>
-              sports
-            </span>
-            <span>
-              prediction
-            </span>
-          </p>
-          <p className="home-case-two-text-2">
-            <span>
-              mobile
-            </span>
-            <span>
-              game
-            </span>
-          </p>
-        </div>
+        hall of famer
         <Link
           className="home-case-two-link"
           to="/hofr"
         >
-          <img
-            alt="A decorative image link that reads: Check it out"
-            className="home-case-one-line-3"
-            src={imgTwo}
-          />
+          View this study
         </Link>
       </section>
 
