be4c8b46e0d2595e932a2b7365ecf3664c39327a
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-06-27 06:59:30 -0700

    added styling

--- assets/js/components/home-case-two.tsx
+++ assets/js/components/home-case-two.tsx
@@ -5,13 +5,37 @@ export default class HomeCaseTwo extends React.Component {
   render() {
     return (
       <section className="home-case-two">
-        hall of famer
-        <Link
-          className="home-case-two-link"
-          to="/hofr"
-        >
-          View this study
-        </Link>
+        <h1 className="home-case-two-heading-1">
+          hall
+        </h1>
+        <h1 className="home-case-two-heading-2">
+          of
+        </h1>
+        <h1 className="home-case-two-heading-3">
+          famer
+        </h1>
+        <div className="home-case-two-link-wrapper">
+          <Link
+            className="home-case-two-link"
+            to="/hofr"
+          >
+            <span className="home-case-two-link-text-1">
+              check
+            </span>
+            <span className="home-case-two-link-text-2">
+              out
+            </span>
+            <span className="home-case-two-link-text-3">
+              this
+            </span>
+            <span className="home-case-two-link-text-4">
+              study
+            </span>
+            <span className="home-case-two-link-arrow">
+              →
+            </span>
+          </Link>
+        </div>
       </section>
 
     )
