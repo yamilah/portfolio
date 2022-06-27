c577bdc8fb3b478a6fabc08919d82065f5c92e59
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-06-27 09:01:06 -0700

    this is super frustrating

--- assets/js/components/home-intro.tsx
+++ assets/js/components/home-intro.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import imgOne from "../../images/home-intro-line-1.svg"
+import imgTwo from "../../images/home-intro-line-3.svg"
 
 export default class HomeIntro extends React.Component {
   render() {
@@ -19,6 +20,38 @@ export default class HomeIntro extends React.Component {
           className="home-intro-line-1"
           src={imgOne}
         />
+        <div className="home-intro-line-2-wrapper">
+          <img
+            alt="A decorative line"
+            className="home-intro-line-2"
+            src={imgTwo}
+          />
+          <p className="home-intro-line-2-text-wrapper">
+            <span className="home-intro-line-2-text-1">
+              <span className="home-intro-line-character-1">
+                des
+              </span>
+              <span className="home-intro-line-character-2">
+                i
+              </span>
+              <span className="home-intro-line-character-3">
+                gn
+              </span>
+              <span className="home-intro-line-character-4">
+                e
+              </span>
+              <span className="home-intro-line-character-5">
+                r
+              </span>
+            </span>
+            <span className="home-intro-line-2-text-2">
+              &
+            </span>
+            <span className="home-intro-line-2-text-3">
+              developer
+            </span>
+          </p>
+        </div>
       </section>
     )
   }
