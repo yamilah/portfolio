3801eb352257194593727a8a6f943a18947e2172
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-07-06 10:18:36 -0700

    added finale styling

--- assets/js/components/case-one-finale.tsx
+++ assets/js/components/case-one-finale.tsx
@@ -4,40 +4,18 @@ export default class CaseOneFinale extends React.Component {
   render() {
     return (
       <section className="case-one-finale">
-        <div className="case-one-finale-separator">
-          <p className="case-one-finale-separator-number">
-            04
-          </p>
-          <p className="case-one-finale-separator-text">
-            Final thoughts
-          </p>
-        </div>
-        <div className="case-one-finale-conclusion">
-          <p className="case-one-finale-conclusion-intro">
-            We launched! We got thousands of users in the first week, which is exactly what we were tracking for. But we also got a temperature check from user feedback and behavior on what a V2 could be.
-          </p>
-          <div className="case-one-finale-conclusion-1">
-            <h1 className="case-one-finale-conclusion-heading">
-              Gamers—in the house
-            </h1>
-            <p className="case-one-finale-conclusion-text">
-              Firstly, EPIC games—the company behind Fortnite—acquired Houseparty after we finished this. Fortnite’s popular in the age group that uses Houseparty—why not do some research around this platform being another way for them to play together, and what that could mean for new design opportunities on Houseparty?
-            </p>
-          </div>
-          <div className="case-one-finale-conclusion-2">
-            <h1 className="case-one-finale-conclusion-heading">
-              Testing easily
-            </h1>
-            <p className="case-one-finale-conclusion-text">
-              Also, I sacrificed convenience of testing for rigor—what I mean by this is that I often tested with people who were accessible to me, like friends and family and, while I could go pretty fast, I had to accept that they weren’t always our representative users, and I’d have to take my learnings with a grain of salt.
-            </p>
-          </div>
-        </div>
-        <div className="case-one-finale-outro">
-          <p className="case-one-finale-outro-text">
-            Thank you!
-          </p>
-        </div>
+        <h1 className="case-one-finale-heading">
+          Any takeaways?
+        </h1>
+        <p className="case-one-finale-text">
+          We launched! There were even articles about it, which was so wild to me. But there were a lot of thoughts I had about the project after the fact. A few things:
+        </p>
+        <p className="case-one-finale-text">
+          Namely, scope. Mobile and desktop were working in lockstep with each other; it worked well, in my opinion, but there was a regular occurrence where we’d all be working on desktop, a new idea would come up, and then we’d have to consistently ask: “do we need this now?” Does mobile need this now? Sometimes, that answer was yes, like in the case with adding DMs. But scope was a huge concern in our work, which was compacted by being such a small team with little access to extensive user data.
+        </p>
+        <p className="case-one-finale-text">
+          But, also, testing. I sacrificed convenience of testing for rigor—what I mean by this is that I often tested with people who were accessible to me, like friends and family and, while I could go pretty fast, I had to accept that they weren’t always our representative users, and I’d have to take my learnings with a grain of salt.
+        </p>
       </section>
     )
   }
