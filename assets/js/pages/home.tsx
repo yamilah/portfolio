400edbbfddfa479ce09cc818a1fbe1c162aa3caf
Yamilah Atallah <yamilah.atallah@gmail.com>
2022-07-12 01:59:53 -0700

    ''''''''''''' uhh

--- assets/js/pages/home.tsx
+++ assets/js/pages/home.tsx
@@ -6,7 +6,7 @@ export default class Home extends React.Component {
     return (
       <section className="home">
         <p className="home-text">
-          Hello. I'm <span className="home-text-emphasis">Yamilah</span>, a <span className="home-text-emphasis">designer & dev</span> in California. I work on <a href="https://blog.twitter.com/en_us/topics/company/2021/introducing-twitter-blue" target="_blank" className="home-text-emphasis home-link">Twitter Blue↗</a> right now. You can view my case studies on <Link to="/houseparty" className="home-text-emphasis home-link">Houseparty↗</Link>, a video chatting social media app, and <Link to="/hofr" className="home-text-emphasis home-link">HOFR↗</Link>, a sports watching companion experience for fans.
+          Hello. I’m <span className="home-text-emphasis">Yamilah</span>, a <span className="home-text-emphasis">designer & dev</span> in California. I work on <a href="https://blog.twitter.com/en_us/topics/company/2021/introducing-twitter-blue" target="_blank" className="home-text-emphasis home-link">Twitter Blue↗</a> right now. You can view my case studies on <Link to="/houseparty" className="home-text-emphasis home-link">Houseparty↗</Link>, a video chatting social media app, and <Link to="/hofr" className="home-text-emphasis home-link">HOFR↗</Link>, a sports watching companion experience for fans.
         </p>
         <aside className="socials">
           <a href="https://linkedin.com/in/yamilah" className="social-link">
