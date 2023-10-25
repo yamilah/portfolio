914c48b08cf3a21f787843dad8a9afd735690f56
Yamilah Atallah <yamilah.atallah@gmail.com>
2023-10-25 00:23:28 -0700

    added fonts, more styling

--- assets/js/intro.tsx
+++ assets/js/intro.tsx
@@ -8,24 +8,38 @@ export default class Intro extends React.Component {
           To whom it may concern:
         </p>
         <div className="intro-name-wrapper">
-          <p className="intro-name-wrapper-1">
+          <p className="intro-name-1">
             ???
           </p>
-          <h1 className="intro-name-wrapper-2">
+          <h1 className="intro-name-2">
             Yamilah
           </h1>
         </div>
         <div className="intro-nickname">
-          <p>
+          <p className="intro-nickname-1">
             AKA "Yam"
           </p>
-          <p>
+          <p className="intro-nickname-2">
             (Nickname OK)
           </p>
         </div>
-        <h1>
+        <h1 className="intro-place">
           California
         </h1>
+        <div className="intro-chachara-wrapper">
+          <p>
+            #
+            <span>
+              305
+            </span>
+          </p>
+          <p>
+            :)
+          </p>
+          <p>
+            HEY
+          </p>
+        </div>
       </section>
     )
   }
